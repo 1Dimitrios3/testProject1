f9277fb3b73f5cf0491df3298cc8f1c1a2237cec
1Dimitrios3 <dimitrismavrokefalos@gmail.com>
2021-04-14 18:26:11 +0300

    fixed unregistered message

--- api/policies/isLoggedIn.js
+++ api/policies/isLoggedIn.js
@@ -3,7 +3,7 @@ module.exports = async function(req, res, proceed) {
     
     if (!req.session.user){
         
-        return res.permissions('Please login to view this content', {when:'isloggedIn policy'},'/login')
+        return res.permissions('Please login to view this content!', {when:'isloggedIn policy'},'/login')
     }
     if (req.session.user.email) {
         //console.log(req.session.user.email)
